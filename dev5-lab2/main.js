7d1d11c9cbf6e548d2910f8c6aafb5594dbf5360
RobinVanOverloop <Robinvo@outlook.com>
2022-10-12 17:37:50 +0200

    feat: add all labo's to one repo

---
+++ dev5-lab2/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+// import another class
+import Bingo from "./bingo.js";
+
+// kickstart the app by calling the constructor via the `new` keyword
+const bingo = new Bingo();
+
