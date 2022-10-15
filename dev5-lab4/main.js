fd15e6ea2649a3e1ecd21aaba623d43816d31267
RobinVanOverloop <Robinvo@outlook.com>
2022-10-15 14:43:15 +0200

    feat: setup main.js with template code

--- dev5-lab4/main.js
+++ dev5-lab4/main.js
@@ -1,23 +1,48 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-setupCounter(document.querySelector('#counter'))
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+      // add orbit controls
+      const controls = new OrbitControls(camera, renderer.domElement);
+
+      // add grid helper
+      const gridHelper = new THREE.GridHelper(200, 50);
+      scene.add(gridHelper);
+
+       // add ambient light
+       const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
+       scene.add( ambientLight );
+
+      // add directional light
+      const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+      directionalLight.position.set( 12, 12, 4);
+      scene.add( directionalLight );
+
+      // add directional light helper
+      const directionalLightHelper = new THREE.DirectionalLightHelper( directionalLight, 2 );
+      scene.add( directionalLightHelper );
+
+      // house
+			const geometry = new THREE.BoxGeometry( 3, 3, 3 );
+			const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+			const cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+
+      // animate function
+			function animate() {
+				requestAnimationFrame( animate );
+
+				
+				renderer.render( scene, camera );
+			};
+
+			animate();
